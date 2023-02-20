cd5975fec28330687679ff8281aa484205460e90
Harry <me@harry.plus>
2023-02-20 16:14:31 +0530

    setup tailwindcss prettier

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -2,6 +2,6 @@
     let name = "World";
 </script>
 
-<h1 class="text-3xl font-bold underline">
+<button class="bg-sky-700 py-2 text-white hover:bg-sky-800 sm:px-4 sm:py-3">
     Hello {name}
-</h1>
+</button>
